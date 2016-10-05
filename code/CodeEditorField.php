0b47f53e4f93b7ae48d474f2a20d157227783f5a
Cochrane, Mike <Mike.Cochrane@airnz.co.nz>
2016-10-05 05:36:04 +0000

    More PSR-2 cleanup

--- code/CodeEditorField.php
+++ code/CodeEditorField.php
@@ -3,88 +3,88 @@
 use SilverStripe\View\Requirements;
 use SilverStripe\Forms\TextareaField;
 
-class CodeEditorField extends TextareaField 
+class CodeEditorField extends TextareaField
 {
 
-	private static $allowed_actions = array (
-		'iframe'
-	);
+    private static $allowed_actions = array (
+        'iframe'
+    );
 
-	/**
-	 * @var string default_mode
-	 */
-	private static $default_mode = 'html';
+    /**
+     * @var string default_mode
+     */
+    private static $default_mode = 'html';
 
-	/**
-	 * @var string default_theme
-	 */
-	private static $default_theme;
-	
-	/**
-	 * @var string mode
-	 */
-	protected $mode;
+    /**
+     * @var string default_theme
+     */
+    private static $default_theme;
 
-	/**
-	 * @var string theme
-	 */
-	protected $theme;
+    /**
+     * @var string mode
+     */
+    protected $mode;
 
-	/**
-	 * @var int Visible number of text lines.
-	 */
-	protected $rows = 8;
+    /**
+     * @var string theme
+     */
+    protected $theme;
+
+    /**
+     * @var int Visible number of text lines.
+     */
+    protected $rows = 8;
 
     public function getAttributes()
     {
-		return array_merge(
-			parent::getAttributes(),
-			array(
-				'data-mode' => $this->getMode(),
-				'data-ace-path' => $this->getAcePath(),
-				'data-theme' => $this->getTheme()
-			)
-		);
-	}
+        return array_merge(
+            parent::getAttributes(),
+            array(
+                'data-mode' => $this->getMode(),
+                'data-ace-path' => $this->getAcePath(),
+                'data-theme' => $this->getTheme()
+            )
+        );
+    }
 
     public function Field($properties = array())
     {
-		$acePath = $this->getAcePath();
+        $acePath = $this->getAcePath();
 
-		Requirements::javascript($acePath . "ace.js");
-		Requirements::javascript($acePath . "mode-" . $this->getMode() . ".js");
-	//	Requirements::javascript($acePath . "worker-" . $this->getMode() . ".js");
+        Requirements::javascript($acePath . "ace.js");
+        Requirements::javascript($acePath . "mode-" . $this->getMode() . ".js");
+    //	Requirements::javascript($acePath . "worker-" . $this->getMode() . ".js");
 
-		Requirements::javascript("codeeditorfield/javascript/CodeEditorField.js");
-		Requirements::css("codeeditorfield/css/CodeEditorField.css");
-		
-		return parent::Field($properties);
-	}
+        Requirements::javascript("codeeditorfield/javascript/CodeEditorField.js");
+        Requirements::css("codeeditorfield/css/CodeEditorField.css");
+
+        return parent::Field($properties);
+    }
 
     public function setMode($mode)
     {
-		$this->mode = $mode;
-		return $this;
-	}
-	
+        $this->mode = $mode;
+        return $this;
+    }
+
     public function getMode()
     {
-		return $this->mode ? $this->mode : $this->config()->get('default_mode');
-	}
+        return $this->mode ? $this->mode : $this->config()->get('default_mode');
+    }
 
     public function setTheme($theme)
     {
-		$this->theme = $theme;
-		return $this;
-	}
-	
+        $this->theme = $theme;
+        return $this;
+    }
+
     public function getTheme()
     {
-		return $this->theme ? $this->theme : $this->config()->get('default_theme');
-	}
+        return $this->theme ? $this->theme : $this->config()->get('default_theme');
+    }
 
     public function getAcePath()
     {
-		return basename(dirname(__DIR__)) . '/thirdparty/ace/src-noconflict/';
-	}
+        return basename(dirname(__DIR__)) . '/thirdparty/ace/src-noconflict/';
+    }
 }
--- code/TinyMCECodeEditor.php
+++ code/TinyMCECodeEditor.php
@@ -8,54 +8,52 @@ use SilverStripe\Forms\FormAction;
 use SilverStripe\Forms\Form;
 use SilverStripe\Control\Controller;
 
-class TinyMCECodeEditor extends Controller 
+class TinyMCECodeEditor extends Controller
 {
 
-	public static $allowed_actions = array (
-		'PopupForm'
-	);
-
+    public static $allowed_actions = array (
+        'PopupForm'
+    );
 
     public function PopupForm()
     {
-		$form = new Form(
-			$this,
-			"{$this->name}/PopupForm", 
-			new FieldList(
-				$headerWrap = new CompositeField(
-					new LiteralField(
-						'Heading', 
-						sprintf('<h3 class="htmleditorfield-mediaform-heading insert">%s</h3>',
-							'Edit Source'
-						)
-					)
-					
-				),
-				$codeField = new CodeEditorField('TinyMCESource', '')
-				
-			),
-			new FieldList(
-				ResetFormAction::create('cancel', 'Cancel')
-					->addExtraClass('ss-ui-action-destructive')
+        $form = new Form(
+            $this,
+            "{$this->name}/PopupForm",
+            new FieldList(
+                $headerWrap = new CompositeField(
+                    new LiteralField(
+                        'Heading',
+                        sprintf(
+                            '<h3 class="htmleditorfield-mediaform-heading insert">%s</h3>',
+                            'Edit Source'
+                        )
+                    )
+                ),
+                $codeField = new CodeEditorField('TinyMCESource', '')
+            ),
+            new FieldList(
+                ResetFormAction::create('cancel', 'Cancel')
+                    ->addExtraClass('ss-ui-action-destructive')
                     ->setUseButtonTag(true),
-				FormAction::create('update', 'Update')
-					->addExtraClass('ss-ui-action-constructive')
-					->setAttribute('data-icon', 'accept')
-					->setUseButtonTag(true)
-			)
-		);
+                FormAction::create('update', 'Update')
+                    ->addExtraClass('ss-ui-action-constructive')
+                    ->setAttribute('data-icon', 'accept')
+                    ->setUseButtonTag(true)
+            )
+        );
 
-		$headerWrap->addExtraClass('CompositeField composite cms-content-header nolabel ');		
-	//	$contentComposite->addExtraClass('tinymce-codeeditor-field content');
+        $headerWrap->addExtraClass('CompositeField composite cms-content-header nolabel ');
+    //	$contentComposite->addExtraClass('tinymce-codeeditor-field content');
 
-		$codeField->addExtraClass('nolabel stacked');
-		
-		$form->unsetValidator();
-		$form->loadDataFrom($this);
-		$form->addExtraClass('htmleditorfield-form htmleditorfield-codeform cms-dialog-content');
-		
-	//	$this->extend('updateLinkForm', $form);
-		
-		return $form;
-	}
+        $codeField->addExtraClass('nolabel stacked');
+
+        $form->unsetValidator();
+        $form->loadDataFrom($this);
+        $form->addExtraClass('htmleditorfield-form htmleditorfield-codeform cms-dialog-content');
+
+    //	$this->extend('updateLinkForm', $form);
+
+        return $form;
+    }
 }
